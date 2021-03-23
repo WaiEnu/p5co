8394f801303527a1d484ba3e1ff8709204550165
WaiEnu <strayhituji@gmail.com>
2021-03-23 18:39:27 +0900

    20210323

--- src/components/Item8.vue
+++ src/components/Item8.vue
@@ -6,29 +6,10 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import p5 from "p5";
 
 export default defineComponent({
   name: "Item8",
   props: {},
-  mounted() {
-    const script = function (p: p5) {
-      p.setup = () => {
-        var canvas = p.createCanvas(600, 300);
-        canvas.parent("item8");
-      };
-
-      p.draw = () => {
-        p.background(0);
-      };
-    };
-
-    new p5(script);
-  },
 });
 </script>
-<style>
-canvas {
-  margin: 0 auto;
-}
-</style>
+<style></style>
